309f505f1964cf997a455b73784ec1ddc96d1d89
marth8880 <marth8880@gmail.com>
2018-08-05 15:16:44 -0700

    Add ObjectFileParser.cs

---
+++ ZeroWorldStats/Modules/ObjectFileParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ZeroWorldStats.Modules
+{
+	public static class ObjectFileParser
+	{
+		
+	}
+}
